9097b66a1a6ab2721f07a9ee1378731e430b17ea
Jack Rong <jackrong03@gmail.com>
2025-05-24 23:55:02 +0100

    run.sh now creates venv automatically

--- run.sh
+++ run.sh
@@ -2,13 +2,21 @@
 
 set -e
 
-#if [ ! -d ".venv" ]; then
-#  echo "Virtual environment not found. Creating a new one..."
-#  python3 -m venv .venv
-#  source .venv/bin/activate
-#  pip install -r requirements.txt
-#  echo "Done. Launching the program..."
-#fi
+if [ ! -d ".venv" ]; then
+  echo "Virtual environment not found. Creating a new one..."
+  python3 -m venv .venv
+  source .venv/bin/activate
+
+  if command -v uv &> /dev/null; then
+    echo "Installing dependencies using uv..."
+    uv pip install -r pyproject.toml
+  else
+    echo "Installing dependencies using pip..."
+    pip install .
+  fi
+
+  echo "Done. Launching the program..."
+fi
 
 source .venv/bin/activate
 PYTHONPATH=. python src/main.py
